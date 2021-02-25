8484faff5931623d5dc2407338b5260c0b4e4249
Sandip Shiwakoti <62270918+sandipshiwakoti@users.noreply.github.com>
2021-02-25 08:25:14 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,594 @@
+const questionCollection = [
+    {
+        "question": "Lunar eclipse occurs on",
+        "a": "Quarter moon",
+        "b": "Full moon",
+        "c": "Half moon",
+        "d": "New moon",
+        "correct": "b"
+    },
+    {
+        "question": "The fastest planet is",
+        "a": "Mercury",
+        "b": "Jupiter",
+        "c": "Saturn",
+        "d": "Mars",
+        "correct": "a"
+    },
+    {
+        "question": "The largest planet of the solar system is",
+        "a": "Pluto",
+        "b": "Saturn",
+        "c": "Mars",
+        "d": "Jupiter",
+        "correct": "d"
+    },
+    {
+        "question": "Parsec is the unit of measurement of",
+        "a": "Orbital velocity of giant stars",
+        "b": "Brightness of heavenly bodies",
+        "c": "Astronomical distance",
+        "d": "Density of stars",
+        "correct": "a"
+    },
+    {
+        "question": "Comets revolve around",
+        "a": "Venus",
+        "b": "Sun",
+        "c": "Earth",
+        "d": "No single heavenly body",
+        "correct": "b"
+    },
+    {
+        "question": "Which planet is known as the Earth’s Twin?",
+        "a": "Neptune",
+        "b": "Mars",
+        "c": "Uranus",
+        "d": "Venus",
+        "correct": "b"
+    },
+    {
+        "question": "The planet having no moon is",
+        "a": "Mercury",
+        "b": "Mars",
+        "c": "Uranus",
+        "d": "Venus",
+        "correct": "a"
+    },
+    {
+        "question": "The distance of the nearest star besides the sun from the earth is",
+        "a": "2 light years",
+        "b": "4 light years",
+        "c": "5 light years",
+        "d": "9 light years",
+        "correct": "b"
+    },
+    {
+        "question": "The unit of measuring distances between the stars is known as",
+        "a": "Light year",
+        "b": "Stellar mile",
+        "c": "Cosmic kilometer",
+        "d": "Galactic unit",
+        "correct": "a"
+    },
+    {
+        "question": "Which one of the following planets takes the same number of days for rotation and revolution?",
+        "a": "Mars",
+        "b": "Jupiter",
+        "c": "Venus",
+        "d": "Mercury",
+        "correct": "d"
+    },
+    {
+        "question": "The bright heavenly body with a long luminous tail is called",
+        "a": "Planet",
+        "b": "Satellite",
+        "c": "Constellation",
+        "d": "Comet",
+        "correct": "D"
+    },
+    {
+        "question": "The planet having the largest number of satellites in the solar system is",
+        "a": "Mars",
+        "b": "Saturn",
+        "c": "Venus",
+        "d": "Mercury",
+        "correct": "b"
+    },
+    {
+        "question": "How many years does Pluto take in revolving round the Sun?",
+        "a": "150 years",
+        "b": "210 years",
+        "c": "115 years",
+        "d": "248 years",
+        "correct": "d"
+    },
+    {
+        "question": "Ursa Minor is",
+        "a": "The name of disease",
+        "b": " The name of a satellite",
+        "c": "A group of stars that form the Little Dipper in the most northern constellation",
+        "d": "The name of an island",
+        "correct": "c"
+    },
+    {
+        "question": "The brightest star in the sky is",
+        "a": "Sun",
+        "b": "Sirius",
+        "c": "Hyades",
+        "d": "Praesepe",
+        "correct": "b"
+    },
+    {
+        "question": "Which one of the following is not a satellite of Jupiter?",
+        "a": "Titan",
+        "b": "Europa",
+        "c": "Callisto",
+        "d": "Io",
+        "correct": "a"
+    },
+    {
+        "question": "Which of the planets is nearest to the earth?",
+        "a": "Mars",
+        "b": "Saturn",
+        "c": "Venus",
+        "d": "Mercury",
+        "correct": "d"
+    },
+    {
+        "question": "Solar eclipse occurs when",
+        "a": "Sun comes between moon and earth",
+        "b": "Moon is at right angle to the earth",
+        "c": "Moon comes between sun and earth",
+        "d": "Earth comes between sun and moon",
+        "correct": "c"
+    },
+    {
+        "question": "The smallest planet is",
+        "a": "Mars",
+        "b": "Saturn",
+        "c": "Venus",
+        "d": "Mercury",
+        "correct": "d"
+    },
+    {
+        "question": "Which of the following is concerned with the description and mapping of the main features of the Universe?",
+        "a": "Cosmology",
+        "b": "Cosmography",
+        "c": "Astronomy",
+        "d": "Geomorphology",
+        "correct": "b"
+    },
+    {
+        "question": "Which planet of the solar system spins on its axis at the fastest rate?",
+        "a": "Jupiter",
+        "b": "Mercury",
+        "c": "Earth",
+        "d": "Saturn",
+        "correct": "a"
+    },
+    {
+        "question": "Which one of the following planets is at the farthest distance from the earth?",
+        "a": "Saturn",
+        "b": "Mars",
+        "c": "Neptune",
+        "d": "Mars",
+        "correct": "c"
+    }
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // },
+    // {
+    //     "question": "",
+    //     "a": "",
+    //     "b": "",
+    //     "c": "",
+    //     "d": "",
+    //     "correct": ""
+    // }
+];
+function shuffle(){
+    questionCollection.forEach((question, index) => {
+        const randomIndex = Math.floor(Math.random() * questionCollection.length);
+        [questionCollection[index], questionCollection[randomIndex]] = [questionCollection[randomIndex], question];
+    });
+}
+shuffle();
+const questions = questionCollection.filter((item, index) => index < 5);
+const quizContainer = document.querySelector(".quiz-container");
+const questionNum = document.querySelector(".question-num");
+const scoreElem = document.querySelector(".score");
+const scoreRecords = document.querySelector(".score-records");
+
+const container = document.querySelector(".container");
+const timerElem = document.querySelector(".timer");
+
+
+let counter = 0;
+let scorePoint = 0;
+let answerFlag = false;
+
+
+// for countdown
+let timeCount, timerId, timeOutId;
+
+// audio timeout
+let audioCountDown = document.querySelector("#audio-count-down");
+
+
+// ./audio/startcountdown.wav
+
+function startCountDown(){
+    timeCount = 10;
+    timerId = setInterval(() => {
+        timeCount--;
+        if(timeCount)
+            timerElem.innerText = `Time left: ${timeCount}s`;
+        else
+            timerElem.innerText = `You missed!`;
+    }, 1000);
+    
+    timeOutId = setTimeout(function fun() {
+        clearInterval(timerId);
+        counter++;
+        audioCountDown.pause();
+        audioCountDown.currentTime = 0;
+        createQuiz();
+    }, 10000);
+}
+
+function createQuiz(){
+    if(counter > questions.length - 1){
+        soundPlay("./audio/SMALL_CROWD_APPLAUSE-Yannick_Lemieux-1268806408.mp3");
+        scoreRecords.style.display = "block";
+        container.style.opacity = "0";
+    }
+    else {
+        quizContainer.innerHTML =
+        `
+            <div class="question-container">
+                <h1 class="question">${questions[counter].question}</h1>
+                <ul class="answers">
+                    <li class="answer" data-option="a">${questions[counter].a}</li>
+                    <li class="answer" data-option="b">${questions[counter].b}</li>
+                    <li class="answer" data-option="c">${questions[counter].c}</li>
+                    <li class="answer" data-option="d">${questions[counter].d}</li>
+                </ul>
+            </div>
+        `;
+        
+        questionNum.innerText = `Question ${counter+1} of ${questions.length}`;
+        
+        setTimeout(()=>{
+            audioCountDown.play();
+        }, 1000);
+        
+        startCountDown();
+    }
+}
+
+document.addEventListener("click", e => {
+    if(e.target.classList.contains("answer")){
+        clearTimeout(timeOutId);
+        clearInterval(timerId);
+        // soundPlay("https://soundbible.com/mp3/Robot_blip-Marianne_Gagnon-120342607.mp3");
+
+        audioCountDown.pause();
+        audioCountDown.currentTime = 0;
+
+        const answerElem = e.target;
+        const optionSelected = e.target.dataset.option
+        
+        if(optionSelected == questions[counter].correct){
+            answerElem.style.background ="green";
+            answerElem.style.color ="white";
+            scorePoint += 25;
+            scoreElem.innerHTML = `Score: ${scorePoint}`;
+            timerElem.innerText = "Correct!";
+            soundPlay("./audio/correct.mp3");
+        }
+        else {
+            answerElem.style.background ="red";
+            answerElem.style.color ="white";
+            timerElem.innerText = "Incorrect!";
+            soundPlay("./audio/disappoint.mp3");
+        }
+        setTimeout(() => {
+            counter++;
+            timerElem.innerText = "Next Question";
+            createQuiz();
+        }, 1000);
+    }
+});
+
+
+const btnSave = document.querySelector(".btn-save");
+const inputUsername = document.querySelector(".username");
+const scoresInfo = document.querySelector(".scores-info");
+
+
+// Local Storage
+// {name: "sandip", score: "200"}
+function getUserScore() {
+    let users;
+    if(!localStorage.getItem("users")){
+        users = [];
+    }
+    else {
+        users = JSON.parse(localStorage.getItem("users"));
+    }
+    return users;
+}
+
+function saveScore() {
+    
+    const name = inputUsername.value.toUpperCase();
+    const score = scorePoint;
+    const user = {name, score};
+
+    let users = getUserScore();
+    let oldUserFlag = false;
+    users = users.map(u => {
+        if(u.name === name){
+            if(score > u.score){
+                u.score = score;
+            }
+            oldUserFlag = true;
+        }
+        return u;
+    });
+    if(!oldUserFlag){
+        users.push(user);
+    }
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+function displayScore(){
+    let users = getUserScore();
+    users.sort((a,b) => b.score - a.score);
+    users = users.map(user => {
+        user= `<p class="score-info">${user.name}: ${user.score}</p>`;
+        return user;
+    }).join("");
+    scoresInfo.innerHTML = users;
+}
+
+btnSave.addEventListener("click", () => {
+    if(inputUsername.value){
+        saveScore();
+        displayScore();
+        btnSave.disabled = true;
+        btnSave.style.cursor = "not-allowed";
+        btnSave.style.background = "red";
+        inputUsername.disabled = true;
+    }
+    else {
+        alert("You have to enter your name to save score!");
+    }
+});
+
+// home
+const quizBox = document.getElementById("quiz-box");
+const homeContainer = document.querySelector("#home-container");
+const btnPlay = document.querySelector(".btn-play");
+const btnPlayAgain = document.querySelector(".btn-play-again");
+const btnExit = document.querySelector(".btn-exit");
+const btnHome = document.querySelector(".btn-home");
+btnExit.addEventListener("click", () => {
+    if (confirm("Are you sure you want to close window?")) {
+        var win = window.open("","_self"); /* url = "" or "about:blank"; target="_self" */
+        win.close();
+    }
+});
+
+
+function soundPlay(url){
+    const sound = document.createElement("audio");
+    sound.src = url;
+    sound.autoplay = true;
+    document.body.appendChild(sound);
+}
+
+btnPlay.addEventListener("click", (e) => {
+    e.preventDefault();
+    homeContainer.style.display = "none";
+    container.style.display = "block";
+    soundPlay("https://soundbible.com/mp3/Robot_blip-Marianne_Gagnon-120342607.mp3");
+    setTimeout(()=>{
+        createQuiz();
+        displayScore();
+    }, 10);
+});
+
+btnPlayAgain.addEventListener("click", () => {
+    homeContainer.style.display = "none";
+    scoreRecords.style.display = "none";
+    container.style.display = "block";
+    soundPlay("https://soundbible.com/mp3/Robot_blip-Marianne_Gagnon-120342607.mp3");
+    setTimeout(()=>{
+        createQuiz();
+        displayScore();
+    }, 10);
+})
+
