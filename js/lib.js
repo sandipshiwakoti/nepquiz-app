0bd45a9a603bc5e8b7493bbc6ac18203147037fd
Sandip Shiwakoti <62270918+sandipshiwakoti@users.noreply.github.com>
2021-02-27 19:11:49 +0545

    module created  for questions

---
+++ js/lib.js
@@ -0,0 +1,642 @@
+export const questionCollection = [
+    {
+        "question": "Lunar eclipse occurs on",
+        "a": "Quarter moon",
+        "b": "Full moon",
+        "c": "Half moon",
+        "d": "New moon",
+        "correct": "b"
+    },
+    {
+        "question": "What is the fastest planet?",
+        "a": "Mercury",
+        "b": "Jupiter",
+        "c": "Saturn",
+        "d": "Mars",
+        "correct": "a"
+    },
+    {
+        "question": "Which planet is known as the Earth’s Twin?",
+        "a": "Neptune",
+        "b": "Mars",
+        "c": "Uranus",
+        "d": "Venus",
+        "correct": "b"
+    },
+    {
+        "question": "If soccer is called football in England, what is American football called in England?",
+        "a": "American football",
+        "b": "Combball",
+        "c": "Handball",
+        "d": "Touchdown",
+        "correct": "a"
+    },
+    {
+        "question": "What is the largest country in the world?",
+        "a": "Russia",
+        "b": "Canada",
+        "c": "United States",
+        "d": "China",
+        "correct": "a"
+    },
+    {
+        "question": "An organic compound is considered an alcohol if it has what functional group?",
+        "a": "Hydroxyl",
+        "b": "Carbonyl",
+        "c": "Alkyl",
+        "d": "Aldehyde",
+        "correct": "a"
+    },
+    {
+        "question": "What is the 100th digit of Pi?",
+        "a": "9",
+        "b": "4",
+        "c": "7",
+        "d": "2",
+        "correct": "a"
+    },
+    {
+        "question": "A doctor with a PhD is a doctor of what?",
+        "a": "Philosophy",
+        "b": "Psychology",
+        "c": "Phrenology",
+        "d": "Physical Therapy",
+        "correct": "a"
+    },
+    {
+        "question": "What year did World War I begin?",
+        "a": "1914",
+        "b": "1905",
+        "c": "1919",
+        "d": "1925",
+        "correct": "a"
+    },
+    {
+        "question": "How many years does Pluto take in revolving round the Sun?",
+        "a": "150 years",
+        "b": "210 years",
+        "c": "115 years",
+        "d": "248 years",
+        "correct": "d"
+    },
+    {
+        "question": "Which is the brightest star in the sky?",
+        "a": "Sun",
+        "b": "Sirius",
+        "c": "Hyades",
+        "d": "Praesepe",
+        "correct": "b"
+    },
+    {
+        "question": "Which one of the following is not a satellite of Jupiter?",
+        "a": "Titan",
+        "b": "Europa",
+        "c": "Callisto",
+        "d": "Io",
+        "correct": "a"
+    },
+    {
+        "question": "Which is the smallest planet?",
+        "a": "Mars",
+        "b": "Saturn",
+        "c": "Venus",
+        "d": "Mercury",
+        "correct": "d"
+    },
+    {
+        "question": "What is isobutylphenylpropanoic acid more commonly known as?",
+        "a": "Ibuprofen",
+        "b": "Morphine",
+        "c": "Ketamine",
+        "d": "Aspirin",
+        "correct": "a"
+    },
+    {
+        "question": "What state is the largest state of the United States of America?",
+        "a": "Alaska",
+        "b": "California",
+        "c": "Texas",
+        "d": "Washington",
+        "correct": "a"
+    },
+    {
+        "question": "What is the tallest mountain in Canada?",
+        "a": "Mount Logan",
+        "b": "Mont Tremblant",
+        "c": "Whistler Mountain",
+        "d": "Blue Mountain",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these is a stop codon in DNA",
+        "a": "TAA",
+        "b": "ACT",
+        "c": "ACA",
+        "d": "GTA",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these countries is NOT a part of the Asian continent?",
+        "a": "Suriname",
+        "b": "Georgia",
+        "c": "Russia",
+        "d": "Singapore",
+        "correct": "a"
+    },
+    {
+        "question": "What is the unit of currency in Laos?",
+        "a": "kip",
+        "b": "Ruble",
+        "c": "Konra",
+        "d": "Dollar",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these animals belongs in class Chondrichthyes?",
+        "a": "Great white shark",
+        "b": "Octopus",
+        "c": "Killer whale",
+        "d": "Catfish",
+        "correct": "a"
+    },
+    {
+        "question": "What is the name of the Canadian national anthem?",
+        "a": "O Canada",
+        "b": "O Red Maple",
+        "c": "Leaf-Spangled Banner",
+        "d": "March of the Puck Drop",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these is NOT an Australian state or territory?",
+        "a": "Alberta",
+        "b": "New South Wales",
+        "c": "Victoria",
+        "d": "Queensland",
+        "correct": "a"
+    },
+    {
+        "question": "In what year did the Wall Street Crash take place?",
+        "a": "1929",
+        "b": "1932",
+        "c": "1930",
+        "d": "1925",
+        "correct": "a"
+    },
+    {
+        "question": "What is considered the rarist form of color blindness?",
+        "a": "Blue",
+        "b": "Red",
+        "c": "Green",
+        "d": "Purple",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these species is not extinct?",
+        "a": "Komodo dragon",
+        "b": "Japanese sea lion",
+        "c": "Tasmanian tiger",
+        "d": "Saudi gazelle",
+        "correct": "a"
+    },
+    {
+        "question": "The emblem on the flag of the Republic of Tajikistan features a sunrise over mountains below what symbol?",
+        "a": "Crown",
+        "b": "Bird",
+        "c": "Sickle",
+        "d": "Tree",
+        "correct": "a"
+    },
+    {
+        "question": "Which ocean borders the west coast of the United States?",
+        "a": "Pacific",
+        "b": "Atlantic",
+        "c": "Indian",
+        "d": "Arctic",
+        "correct": "a"
+    },
+    {
+        "question": "All of the following human genetic haplogroup names are shared between Y-chromosome and mitochondrial DNA haplogroups EXCEPT",
+        "a": "Haplogroup U",
+        "b": "Haplogroup L",
+        "c": "Haplogroup T",
+        "d": "Haplogroup J",
+        "correct": "a"
+    },
+    {
+        "question": "What country saw a world record 315 million voters turn out for elections on May 20, 1991?",
+        "a": "India",
+        "b": "USA",
+        "c": "Soviet Union",
+        "d": "Poland",
+        "correct": "a"
+    },
+    {
+        "question": "When did a NASA rover named 'Perseverance' successfully land on the Martian surface?",
+        "a": "29th January 2021",
+        "b": "18th February 2021",
+        "c": "10th February 2021",
+        "d": "5th February 2021",
+        "correct": "b"
+    },
+    {
+        "question": "Which is the largest freshwater lake in the world?",
+        "a": "Lake Superior",
+        "b": "Caspian Sea",
+        "c": "Lake Michigan",
+        "d": "Lake Huron",
+        "correct": "a"
+    },
+    {
+        "question": "What name was historically used for the Turkish city currently known as Istanbul?",
+        "a": "Constaninople",
+        "b": "Hüdavendigar",
+        "c": "Söğüt",
+        "d": "Adrianople",
+        "correct": "a"
+    },
+    {
+        "question": "How many legs is it biologically impossible for a centipede to have?",
+        "a": "100",
+        "b": "26",
+        "c": "50",
+        "d": "74",
+        "correct": "a"
+    },
+    {
+        "question": "What is the scientific name of the knee cap?",
+        "a": "Patella",
+        "b": "Foramen Magnum",
+        "c": "Scapula",
+        "d": "Anura",
+        "correct": "a"
+    },
+    {
+        "question": "What physics principle relates the net electric flux out of a closed surface to the charge enclosed by that surface?",
+        "a": "Gauss's Law",
+        "b": "Faraday's Law",
+        "c": "Ampere's Law",
+        "d": "Biot-Savart Law",
+        "correct": "a"
+    },
+    {
+        "question": "What is the fastest land animal?",
+        "a": "Cheetah",
+        "b": "Lion",
+        "c": "Tiger",
+        "d": "Zebra",
+        "correct": "a"
+    },
+    {
+        "question": "What is the Italian word for 'tomato'?",
+        "a": "Pomodoro",
+        "b": "Aglio",
+        "c": "Cipolla",
+        "d": "Peperoncino",
+        "correct": "a"
+    },
+    {
+        "question": "Botanically speaking, which of these fruits is NOT a berry?",
+        "a": "Strawberry",
+        "b": "Blueberry",
+        "c": "Concord Grape",
+        "d": "Banana",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these is NOT a province in China?",
+        "a": "Yangtze",
+        "b": "Fujian",
+        "c": "Sichuan",
+        "d": "Guangdong",
+        "correct": "a"
+    },
+    {
+        "question": "In a 1994 CBS interview, Microsoft co-founder Bill Gates performed what unusual trick on camera?",
+        "a": "Jumping over an office chair",
+        "b": "Jumping backwards over a desk",
+        "c": "Standing on his head",
+        "d": "Typing on a keyboard during a handstand",
+        "correct": "a"
+    },
+    {
+        "question": "What is the oldest Disney film?",
+        "a": "Snow White and the Seven Dwarfs",
+        "b": "Pinocchio",
+        "c": "Dumbo",
+        "d": "Fantasia",
+        "correct": "a"
+    },
+    {
+        "question": "Which scientific unit is named after an Italian nobleman?",
+        "a": "Volt",
+        "b": "Hertz",
+        "c": "Ohm",
+        "d": "Pascal",
+        "correct": "a"
+    },
+    {
+        "question": "What is the molecular formula of Ozone?",
+        "a": "N20",
+        "b": "O3",
+        "c": "C6H206",
+        "d": "SO4",
+        "correct": "b"
+    },
+    {
+        "question": "What is the name of a rabbit's abode?",
+        "a": "Burrow",
+        "b": "Nest",
+        "c": "Den",
+        "d": "Dray",
+        "correct": "a"
+    },
+    {
+        "question": "Which of the following is NOT a real element?",
+        "a": "Vitrainium",
+        "b": "Praseodymium",
+        "c": "Hassium",
+        "d": "Lutetium",
+        "correct": "a"
+    },
+    {
+        "question": "Harvard University is located in which city?",
+        "a": "Cambridge",
+        "b": "Providence",
+        "c": "New York",
+        "d": "Washington D.C.",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these is the name of a Japanese system of alternative medicine, literally meaning 'finger pressure'?",
+        "a": "Shiatsu",
+        "b": "Ukiyo",
+        "c": "Majime",
+        "d": "Ikigai",
+        "correct": "a"
+    },
+    {
+        "question": "Which movie contains the quote, 'Say hello to my little friend!'",
+        "a": "Scarface",
+        "b": "Reservoir Dogs",
+        "c": "Heat",
+        "d": "Goodfellas",
+        "correct": "a"
+    },
+    {
+        "question": "What is the capital of Indonesia?",
+        "a": "Jakarta",
+        "b": "Bandung",
+        "c": "Medan",
+        "d": "Palembang",
+        "correct": "a"
+    },
+    {
+        "question": "Which of these chemical compounds is NOT found in gastric acid?",
+        "a": "Sulfuric acid",
+        "b": "Hydrochloric acid",
+        "c": "Potassium chloride",
+        "d": "Sodium chloride",
+        "correct": "a"
+    },
+    {
+        "question": "George Orwell wrote this book, which is often considered a statement on government oversight.",
+        "a": "1984",
+        "b": "The Old Man and the Sea",
+        "c": "Catcher and the Rye",
+        "d": "To Kill a Mockingbird",
+        "correct": "a"
+    },
+    {
+        "question": "Which country was NOT formerly part of Yugoslavia?",
+        "a": "Albania",
+        "b": "Croatia",
+        "c": "Serbia",
+        "d": "Macedonia",
+        "correct": "a"
+    },
+    {
+        "question": "Who wrote 'Harry Potter?'",
+        "a": "Terry Pratchett",
+        "b": "J.R.R. Tolkien",
+        "c": "J.K. Rowling",
+        "d": "Daniel Radcliffe",
+        "correct": "c"
+    },
+    {
+        "question": "Which city is the capital of the United States of America?",
+        "a": "Washington D.C",
+        "b": "Seattle",
+        "c": "Albany",
+        "d": "Los Angeles",
+        "correct": "a"
+    },
+    {
+        "question": "The idea of Socialism was articulated and advanced by whom?",
+        "a": "Karl Marx",
+        "b": "Vladimir Lenin",
+        "c": "Joseph Stalin",
+        "d": "Adolf Hitler",
+        "correct": "a"
+    },
+    {
+        "question": "What is the name given to Indian food cooked over charcoal in a clay oven?",
+        "a": "Tandoori",
+        "b": "Biryani",
+        "c": "Pani puri",
+        "d": "Tiki masala",
+        "correct": "a"
+    },
+    {
+        "question": "What is the capital of Scotland?",
+        "a": "Edinburgh",
+        "b": "GlasgowDundee",
+        "c": "London",
+        "d": "Dundee",
+        "correct": "a"
+    },
+    {
+        "question": "What name represents the letter 'M' in the NATO phonetic alphabet?",
+        "a": "Mike",
+        "b": "Matthew",
+        "c": "Mark",
+        "d": "Max",
+        "correct": "a"
+    },
+    {
+        "question": "How many Harry Potter books are there?",
+        "a": "7",
+        "b": "8",
+        "c": "5",
+        "d": "6",
+        "correct": "a"
+    },
+    {
+        "question": "What is the currency of Poland?",
+        "a": "Złoty",
+        "b": "Ruble",
+        "c": "Euro",
+        "d": "Krone",
+        "correct": "a"
+    },
+    {
+        "question": "What is the area of Vatican City?",
+        "a": "0.44km^2",
+        "b": "0.10km^2",
+        "c": "0.86km^2",
+        "d": "12.00km^2",
+        "correct": "a"
+    },
+    {
+        "question": "What genetic disease is caused by having an extra Y chromosome (XYY)?",
+        "a": "Jacob's Syndrome",
+        "b": "Klinefelter's Syndrome",
+        "c": "Turner's Syndrome",
+        "d": "Down Syndrome",
+        "correct": "a"
+    },
+    {
+        "question": "What was the first sport to have been played on the moon?",
+        "a": "Golf",
+        "b": "Football",
+        "c": "Tennis",
+        "d": "Soccer",
+        "correct": "a"
+    },
+    {
+        "question": "In the movie 'Blade Runner', what is the term used for human-like androids ?",
+        "a": "Replicants",
+        "b": "Cylons",
+        "c": "Synthetics",
+        "d": "Skinjobs",
+        "correct": "a"
+    },
+    {
+        "question": "The derisive acronym 'PIIGS' refers to which of the following European countries and their economic statuses?",
+        "a": "Portugal, Ireland, Italy, Greece, Spain",
+        "b": "Poland, Iceland, Italy, Greece, Serbia",
+        "c": "Poland, Iceland, Italy, Greenland, Spain",
+        "d": "Portugal, Iceland, Ireland, Greece, Serbia",
+        "correct": "a"
+    },
+    {
+        "question": "Which nation claims ownership of Antarctica?",
+        "a": "No one, but there are claims.",
+        "b": "United States of America",
+        "c": "United Nations",
+        "d": "Australia",
+        "correct": "a"
+    },
+    {
+        "question": "What country is the second largest in the world by area?",
+        "a": "Canada",
+        "b": "Russia",
+        "c": "China",
+        "d": "USA",
+        "correct": "a"
+    },
+    {
+        "question": "The Hunua Ranges is located in...",
+        "a": "New Zealand",
+        "b": "Nepal",
+        "c": "China",
+        "d": "Mexico",
+        "correct": "a"
+    },
+    {
+        "question": "Which of the following is NOT a word used to describe an earthquake?",
+        "a": "Drop-slide",
+        "b": "Foreshock",
+        "c": "Strike-slip",
+        "d": "Temblor",
+        "correct": "a"
+    },
+    {
+        "question": "What is the capital city of Bermuda?",
+        "a": "Hamilton",
+        "b": "Santo Dominigo",
+        "c": "San Juan",
+        "d": "Havana",
+        "correct": "a"
+    }
+    ,{
+        "question": "How many objects are equivalent to one mole?",
+        "a": "6.022 x 10^23",
+        "b": "6.002 x 10^22",
+        "c": "6.022 x 10^22",
+        "d": "6.002 x 10^23",
+        "correct": "a"
+    }
+    ,{
+        "question": "Who starred in the film 1973 movie 'Enter The Dragon'?",
+        "a": "Bruce Lee",
+        "b": "Jackie Chan",
+        "c": "Jet Li",
+        "d": "Yun-Fat Chow",
+        "correct": "a"
+    },
+    {
+        "question": "Leonardo Di Caprio won his first Best Actor Oscar for his performance in which film?",
+        "a": "Shutter Island",
+        "b": "Inception",
+        "c": "The Revenant",
+        "d": "Titanic",
+        "correct": "c"
+    },
+    {
+        "question": "Where is the fast food chain 'Panda Express' headquartered?",
+        "a": "Rosemead, California",
+        "b": "Sacramento, California",
+        "c": "Fresno, California",
+        "d": "San Diego, California",
+        "correct": "a"
+    },
+    {
+        "question": " What is the national bird of Nepal called?",
+        "a": "Himalayan Monal",
+        "b": "Spiny babbler",
+        "c": "Kalij Pheasant",
+        "d": "Bulbul",
+        "correct": "a"
+    },
+    {
+        "question": "Which is a country with no police and no prison",
+        "a": "Greenland",
+        "b": "Iceland",
+        "c": "Switzerland",
+        "d": "Jordan",
+        "correct": "b"
+    },
+    {
+        "question": "Which mountain of Nepal is considered as the basis for authentic time?",
+        "a": "Gaurishankar",
+        "b": "Everest",
+        "c": "Kanchenjunga",
+        "d": "Manaslu",
+        "correct": "a"
+    },
+    {
+        "question": "Who holds the record for taking the shortest time to climb all 14 of the world's 8,000-meter-high mountains?",
+        "a": "Pemba Dorje Sherpa",
+        "b": "Nirmal Purja",
+        "c": "Mingma Sherpa",
+        "d": "Ueli Steck",
+        "correct": "b"
+    },
+    {
+        "question": "Who is the first person to free solo 'El Capitan'?",
+        "a": "Jimmy Jewell",
+        "b": "Brad Gobright",
+        "c": "Cayan Tower",
+        "d": "Alex Honnold",
+        "correct": "d"
+    },
+    {
+        "question": "What is the name of video game created by Elon Musk at the age of 12 which later sold for $500?",
+        "a": "Doom",
+        "b": "Dodge",
+        "c": "Blaster",
+        "d": "Cyberpunk",
+        "correct": "c"
+    }
+];
